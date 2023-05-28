4c130ae3685e545cd6957b62e54f3c525756a00e
Reyson Silva <reysonsillva@gmail.com>
2023-05-27 23:19:58 -0300

    variaveis

---
+++ javascript/variaveis.js
@@ -0,0 +1,32 @@
+// Variáveis
+
+let idade = 26
+
+console.log(idade)
+
+// Tipos de variáveis
+
+idade = 30                      // Number
+let numero = 30                 // Number
+let altura = 1.77               // Number  
+let nome = 'Reyson'             // String (texto)
+let estudando = true            // Booleano (true ou false)
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra
+
+console.log(outra)
+
+let nula = null
+
+console.log(null)
+
+var variavel = 30
+
+console.log(variavel)
+
+const constante = 100
