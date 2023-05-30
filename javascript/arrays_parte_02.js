11a100849f1b569b2ab61b92841b95a6cabf1147
Reyson Silva <reysonsillva@gmail.com>
2023-05-29 21:35:36 -0300

    arrays

--- javascript/arrays_parte_02.js
+++ javascript/arrays_parte_02.js
@@ -48,12 +48,40 @@ console.clear()
 
 //  Concatenando arrays: concat
 
+console.log('arr1', arr1)
+console.log('arr2', arr2)
 
+console.log(arr1.concat(arr2))
+console.log(arr2.concat(arr1))
+
+console.clear()
 
 //  Buscando elementos: indexOf  | lastIndesOf
 
+console.log(arr1)
+
+let indiceDoElemento34 = arr1.indexOf(34)
+
+console.log(indiceDoElemento34)
+
+let arr3 = [1, 2, 3, 3, 5, 3]
+
+console.log(arr3.lastIndexOf(3))
+
+console.clear()
+
 //  Descobrindo a existência de um elemento: includes
+
+console.log(arr1)
+
+console.log(arr1.includes(12))
+
+console.clear()
 
 //  Invertendo arrays: reverse
 
-//  
+console.log('arr1 normal:', arr1)
+
+const arr1Invertido = arr1.reverse()
+
+console.log('arr1 invertido:', arr1Invertido)
