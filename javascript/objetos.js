18de0d8102cb9e9075c9751aaa3d951098695fb1
Reyson Silva <reysonsillva@gmail.com>
2023-05-30 21:03:25 -0300

    final.javascript

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -2,11 +2,33 @@
 
 // Como criar um objeto no JavaScript?
 
+let pessoa = {
+    nome: 'Reyson',
+    idade: 23,
+}
 
+console.log(pessoa)
+
+console.log(pessoa['nome'])
+console.log(pessoa.idade)
 
 // Como adicionar um par chave-valor?
 
+pessoa.altura = 1.74
+
+console.log(pessoa)
+
 // Como remover um par chave-valor?
 
+delete pessoa.altura
+
+console.log(pessoa)
+
+console.clear()
+
 // Como percorrer?
+
+for (let chave in pessoa) {
+    console.log(chave)
+}
 
